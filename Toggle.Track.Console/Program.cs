5df7b2de98ffe8a3fa00b2221a180da99e808c4a
Sven Vranckx <sven.vranckx@gmail.com>
2024-03-28 14:49:01 +0100

    Minimal HTTP client

---
+++ Toggle.Track.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Toggle.Track.SDK;
+
+namespace Toggle.Track
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter API token:");
+            var token = Console.ReadLine();
+
+            using var client = new Client(token!);
+            var response = await client.GetString("/api/v9/me/time_entries");
+
+            Console.WriteLine(response);
+        }
+    }
+}
---
+++ Toggle.Track.SDK/Client.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Toggle.Track.SDK
+{
+    public class Client : IDisposable
+    {
+        private readonly HttpClient _httpClient;
+        private bool _disposed;
+
+        private const string BaseUrl = @"https://api.track.toggl.com";
+
+        public Client(string apiToken)
+        {
+            var handler = new HttpClientHandler();
+            _httpClient = new HttpClient(handler, true)
+            {
+                BaseAddress = new Uri(BaseUrl)
+            };
+
+            var credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{apiToken}:api_token"));
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+        }
+
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+            _disposed = true;
+            _httpClient.Dispose();
+        }
+
+        public async Task<string> GetString(string query)
+        {
+            return await _httpClient.GetStringAsync(query);
+        }
+    }
+}
